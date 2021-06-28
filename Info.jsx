cb2e76e3d3ac5339a77cf2b8831afcdade3ce220
vishnu12s <85442973+vishnu12s@users.noreply.github.com>
2021-06-28 12:22:14 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info(){
+   return (
+     <div class= "note">
+       <h1>javascript and react .js</h1>
+       <p> A basic web development program </p>
+       </div>
+    
+       
+   );
+   
+   }
+export default Info;
+   
